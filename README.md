fc4ab0eb66f9d630d4d5f088be784cdf71470c03
marty <martymagaan@gmail.com>
2018-06-06 10:18:43 -0400

    Change name and other minor changes

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
-# Vertical Scrollbox
+# V Scrollbox
 
 A simple, customizable, pure javascript, cross-browser vertical scrollbox.
 
-**See [demo.html](http://projects.martymagaan.com/vertical-scrollbox/demo.html) file to see it in action.**
+**See [demo.html](http://projects.martymagaan.com/v-scrollbox/demo.html) file to see it in action.**
 
 ## How to use
 
 Link the css file in your HTML (replace *path-to* with appropriate path):
 
-    <link rel="stylesheet" type="text/css" href="path-to/vertical-scrollbox.css">
+    <link rel="stylesheet" type="text/css" href="path-to/v-scrollbox.css">
 
 Add the javascript file to the bottom of your HTML file before closing the body tag (replace *path-to* with appropriate path):
 
-    <script src="path-to/vertical-scrollbox.js"></script>
+    <script src="path-to/v-scrollbox.js"></script>
 
 Add *v-scrollbox* class to any elements you want to turn into a scrollbox,
 and wrap the contents inside it with a *v-scrollbox-content* div, as shown below:
@@ -26,7 +26,7 @@ and wrap the contents inside it with a *v-scrollbox-content* div, as shown below
 The *myDiv* element in the example above should have a set height.
 The scrollbars will only appear if the content overflows past the height of the div.
 
-You can customize the look of the scrollbar in the *vertical-scrollbox.css* file.
+You can customize the look of the scrollbar in the *v-scrollbox.css* file.
 
 **For dynamically created content** you can use the global *initScrollBox()* function everytime dynamic 
 content is added to activate scrollbars if the newly added content causes the need for scrollbars.
@@ -38,21 +38,18 @@ content is added to activate scrollbars if the newly added content causes the ne
 * Chrome 31+
 * FireFox 17+
 * IE 10+
+* Safari 7+
 * Opera 18+
 
-*Uses native scrollbar for Safari*
+Compatibility is based on wheel event compatibility.
 
 *Important note concerning IE and Edge Browsers*
 
-For IE and Edge, this plugin falls back to an autohiding native overflow-y scroll due to an obscure issue where 
-mouse wheel events are not captured when using 2-finger touchpad scrolling on Windows 10 devices that use a 
-Precision touchpad. It does not seem like Microsoft will be fixing this issue as it only affects a small number of users. 
+For IE and Edge, there is an obscure issue where mouse wheel events are not captured when using 
+2-finger touchpad scrolling on Windows 10 devices that use a Precision touchpad. It does not seem 
+like Microsoft will be fixing this issue as it only affects a small number of users. 
 More details about it here: https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/7134034
 
-If you don't care about this small percentage of users, and you wish to disable this fallback feature and just use
-the regular custom scrollbar functionality of this plugin (which works on IE 9+ and Edge except in the case 
-mentioned above), set the *useIEEdgeFallBack* variable to *false* in the js file.
-
 ## TO DO
-* Better IE/Edge Windows 10 Precision touchpad fallback
+* Make IE/Edge Windows 10 Precision touchpad fallback
 * Add touchscreen scrolling capability
--- v-scrollbox.js
+++ v-scrollbox.js
@@ -1,8 +1,9 @@
-var initScrollBox;
+let initScrollBox;
 
 (function () {
-  var maxPixelsPerScroll = 80;
-  var useIEEdgeFallBack = true;
+  let maxPixelsPerScroll = 80;
+  let useIEEdgeFallBack = true;
+  let scrollBoxes;
 
   initScrollBox = function() {
     setScrollBars();
@@ -14,16 +15,15 @@ var initScrollBox;
   window.onload = initScrollBox;
   window.addEventListener('resize', initScrollBox);
 
-  var scrollBoxes;
   function setScrollBars() {
     scrollBoxes = document.getElementsByClassName('v-scrollbox');
-    for(var i = 0; i < scrollBoxes.length; i++)
+    for(let i = 0; i < scrollBoxes.length; i++)
       createScrollBarIfOverflow(scrollBoxes[i]);
   }
   function createScrollBarIfOverflow(scrollBox) {
-    var boxHeight = scrollBox.clientHeight;
-    var contentHeight = scrollBox.scrollHeight;
-    if(contentHeight > boxHeight && !isIEorEdge() && !isSafari()) {
+    let boxHeight = scrollBox.clientHeight;
+    let contentHeight = scrollBox.scrollHeight;
+    if(contentHeight > boxHeight) {
       addScrollBarIfNonexistent(scrollBox);
       setScrollBarDimensions(scrollBox, boxHeight, contentHeight);
     }
@@ -39,29 +39,29 @@ var initScrollBox;
   }
   function addScrollBarIfNonexistent(scrollBox) {
     if(!checkIfScrollBarExists(scrollBox)) {
-      var scrollbar = document.createElement('div');
+      let scrollbar = document.createElement('div');
       scrollbar.className = 'v-scrollbar';
       scrollBox.insertBefore(scrollbar, scrollBox.firstChild);
     }
   }
   function setScrollBarDimensions(scrollBox, boxHeight, contentHeight) {
-    var scrollBar = scrollBox.children[0];
-    var scrollBarSize = calculateScrollBarSize(boxHeight, contentHeight);
+    let scrollBar = scrollBox.children[0];
+    let scrollBarSize = calculateScrollBarSize(boxHeight, contentHeight);
     scrollBar.style.height = scrollBarSize + 'px';
     if(scrollBox.children[1].yPos == null) scrollBox.children[1].yPos = 0;
     setScrollLimits(scrollBox);
     enforceLimitsOnResize(scrollBox);
   }
   function calculateScrollBarSize(boxHeight, contentHeight) {
-    var scrollBarSize = (boxHeight * boxHeight)/contentHeight;
+    let scrollBarSize = (boxHeight * boxHeight)/contentHeight;
     if(scrollBarSize < 100) scrollBarSize = 100;
     return scrollBarSize;
   }
   function setScrollLimits(scrollBox) {
-    var scrollBar = scrollBox.children[0];
-    var content = scrollBox.children[1];
-    var marginBottom = getStyleInt(scrollBar, 'margin-bottom');
-    var boxHeight = scrollBox.clientHeight;
+    let scrollBar = scrollBox.children[0];
+    let content = scrollBox.children[1];
+    let marginBottom = getStyleInt(scrollBar, 'margin-bottom');
+    let boxHeight = scrollBox.clientHeight;
     scrollBar.maxScrollBarY = boxHeight - scrollBar.clientHeight - (marginBottom * 2);
     content.minContentY = -(scrollBox.scrollHeight - boxHeight);
   }
@@ -75,9 +75,9 @@ var initScrollBox;
     return window.getComputedStyle(element).getPropertyValue(style);
   }
   function enforceLimitsOnResize(scrollBox) {
-    var scrollBar = scrollBox.children[0];
-    var content = scrollBox.children[1];
-    var contentY = getStyleInt(content, 'top');
+    let scrollBar = scrollBox.children[0];
+    let content = scrollBox.children[1];
+    let contentY = getStyleInt(content, 'top');
     if(contentY < content.minContentY) {
       content.style.top = content.minContentY + 'px';
       content.yPos = content.minContentY;
@@ -93,7 +93,7 @@ var initScrollBox;
   }
 
   function addScrollFunctionality() {
-    for(var i = 0; i < scrollBoxes.length; i++)
+    for(let i = 0; i < scrollBoxes.length; i++)
       addListenersIfScrollable(scrollBoxes[i]);
   }
   function addListenersIfScrollable(scrollBox) {
@@ -103,16 +103,15 @@ var initScrollBox;
       addScrollListener(scrollBox);
       addDragListener(scrollBox);
     }
-    else if(isIEorEdge() || isSafari()) useOverflowScroll(scrollBox);
   }
-  var focusedScrollBox;
+  let focusedScrollBox;
   function addFocusListener(scrollBox) {
     scrollBox.addEventListener('mouseenter', setFocusedScrollBox);
     scrollBox.addEventListener('mouseleave', setFocusedScrollBox);
   }
   function setFocusedScrollBox() {
     setTimeout(function() {
-      var hovered = document.querySelectorAll('.v-scrollbox:hover');
+      let hovered = document.querySelectorAll('.v-scrollbox:hover');
       focusedScrollBox = hovered[hovered.length - 1];
     }, 1);
   }
@@ -142,10 +141,8 @@ var initScrollBox;
   }
 
   function addScrollListener(scrollBox) {
-    var event = findSupportedWheelEvent(scrollBox);
-    if(scrollBox.addEventListener)
-      scrollBox.addEventListener(event, mouseWheelHandler, false);
-    else scrollBox.attachEvent('onmousewheel', mouseWheelHandler);
+    let event = findSupportedWheelEvent(scrollBox);
+    scrollBox.addEventListener(event, mouseWheelHandler, false);
   }
   function findSupportedWheelEvent(scrollBox) {
     return 'onwheel' in scrollBox ? 'wheel' :
@@ -153,8 +150,8 @@ var initScrollBox;
   }
   function mouseWheelHandler(e) {
     if(checkIfScrollBarExists(this)) {
-      var delta = normalizeDelta(-e.deltaY);
-      var scrollBox = focusedScrollBox == undefined ? this : focusedScrollBox;
+      let delta = normalizeDelta(-e.deltaY);
+      let scrollBox = focusedScrollBox == undefined ? this : focusedScrollBox;
       scrollViaMouseWheel(scrollBox, delta);
     }
   }
@@ -170,42 +167,43 @@ var initScrollBox;
     return delta;
   }
   function scrollViaMouseWheel(scrollBox, delta) {
-    var content = scrollBox.children[1];
+    let content = scrollBox.children[1];
     content.yPos += delta;
     keepScrollInBounds(delta, content);
     smoothScrollIfHighDelta(delta, content);
   }
   function keepScrollInBounds(delta, content) {
-    if(delta < 0 && content.yPos < content.minContentY) content.yPos = content.minContentY;
+    if(delta < 0 && content.yPos < content.minContentY)
+      content.yPos = content.minContentY;
     else if(delta > 0 && content.yPos > 0) content.yPos = 0;
   }
   function smoothScrollIfHighDelta(delta, content) {
     if(delta >= 2 || delta <= -2) smoothScroll(content);
     else {
       content.style.top = content.yPos + 'px';
-      var newScrollBarY = calculateNewScrollBarY(content, content.yPos);
+      let newScrollBarY = calculateNewScrollBarY(content, content.yPos);
       moveScrollBar(content.parentElement, newScrollBarY);
     }
   }
-  var intervalRunning = false;
+  let intervalRunning = false;
   function smoothScroll(content) {
-    var diff = content.yPos - getStyleInt(content, 'top');
-    var direction = (diff == 0) ? 0 : diff/Math.abs(diff);
+    let diff = content.yPos - getStyleInt(content, 'top');
+    let direction = (diff == 0) ? 0 : diff/Math.abs(diff);
     if(!intervalRunning) startSmoothScrollInterval(content, direction);
   }
   function startSmoothScrollInterval(content, direction) {
     intervalRunning = true;
     content.smoothInterval = setInterval(function() {
-      var transitionY = calculateTransitionY(content, direction);
+      let transitionY = calculateTransitionY(content, direction);
       content.style.top = transitionY + 'px';
-      var newScrollBarY = calculateNewScrollBarY(content, transitionY);
+      let newScrollBarY = calculateNewScrollBarY(content, transitionY);
       moveScrollBar(content.parentElement, newScrollBarY);
     }, 16);
   }
   function calculateTransitionY(content, direction) {
-    var currentY = getStyleInt(content, 'top');
+    let currentY = getStyleInt(content, 'top');
     smoothScrollSpeed = Math.max(2, Math.abs(content.yPos - currentY) * 0.23);
-    var transitionY = currentY + (direction * smoothScrollSpeed);
+    let transitionY = currentY + (direction * smoothScrollSpeed);
     if((direction >= 0 && transitionY >= content.yPos) ||
     (direction <= 0 && transitionY <= content.yPos) ||
     content.style.top == undefined) {
@@ -215,11 +213,11 @@ var initScrollBox;
     } else return transitionY;
   }
   function calculateNewScrollBarY(content, contentY) {
-    var scrollBox = content.parentElement;
+    let scrollBox = content.parentElement;
     return (contentY * scrollBox.children[0].maxScrollBarY) / content.minContentY;
   }
   function moveScrollBar(scrollBox, newY) {
-    var scrollBar = scrollBox.children[0];
+    let scrollBar = scrollBox.children[0];
     if(newY < 0) newY = 0;
     else if(newY > scrollBar.maxScrollBarY) newY = scrollBar.maxScrollBarY;
     scrollBar.style.top = newY + 'px';
@@ -228,10 +226,10 @@ var initScrollBox;
   function addDragListener(scrollBox) {
     scrollBox.children[0].onmousedown = dragScrollBarInit;
   }
-  var scrollBarIsGrabbed = false;
-  var grabbedScrollBar;
-  var scrollBarOffset;
-  var scrollBoxContentOffset;
+  let scrollBarIsGrabbed = false;
+  let grabbedScrollBar;
+  let scrollBarOffset;
+  let scrollBoxContentOffset;
   function dragScrollBarInit(e) {
     scrollBarIsGrabbed = true;
     grabbedScrollBar = this;
@@ -239,15 +237,15 @@ var initScrollBox;
     disableContentSelect();
   }
   function calculateScrollBarOffset(e) {
-    var initialMouseY = document.all ? window.event.clientY : e.pageY;
-    var scrollBarY = grabbedScrollBar.offsetTop;
+    let initialMouseY = document.all ? window.event.clientY : e.pageY;
+    let scrollBarY = grabbedScrollBar.offsetTop;
     scrollBarOffset = initialMouseY - scrollBarY;
   }
   function disableContentSelect() {
     addClass(grabbedScrollBar.parentElement, 'disable-select');
   }
   function addClass(element, className) {
-    var classes = element.className.split(' ');
+    let classes = element.className.split(' ');
     if(classes.indexOf(className) == -1)
       element.className += ' ' + className;
   }
@@ -256,18 +254,18 @@ var initScrollBox;
 
   function dragScrollBar(e) {
     if(scrollBarIsGrabbed) {
-      var newScrollBarY = calculateScrollBarDragY(e);
-      var newScrollBoxContentY = calculateNewContentY(grabbedScrollBar, newScrollBarY);
+      let newScrollBarY = calculateScrollBarDragY(e);
+      let newScrollBoxContentY = calculateNewContentY(grabbedScrollBar, newScrollBarY);
       moveScrollBarViaDrag(newScrollBarY);
       moveScrollBoxContent(grabbedScrollBar.parentElement, newScrollBoxContentY);
     }
   }
   function calculateScrollBarDragY(e) {
-    var mouseY = document.all ? window.event.clientY : e.pageY;
+    let mouseY = document.all ? window.event.clientY : e.pageY;
     return mouseY - scrollBarOffset;
   }
   function calculateNewContentY(scrollBar, scrollBarY) {
-    var scrollBox = scrollBar.parentElement;
+    let scrollBox = scrollBar.parentElement;
     return (scrollBarY * scrollBox.children[1].minContentY) / scrollBar.maxScrollBarY;
   }
   function moveScrollBarViaDrag(newY) {
@@ -277,7 +275,7 @@ var initScrollBox;
     grabbedScrollBar.style.top = newY + 'px';
   }
   function moveScrollBoxContent(scrollBox, newY) {
-    var content = scrollBox.children[1];
+    let content = scrollBox.children[1];
     if(newY > 0) newY = 0;
     else if(newY < content.minContentY) newY = content.minContentY;
     content.style.top = newY + 'px';
@@ -292,34 +290,23 @@ var initScrollBox;
   }
   function enableContentSelect() {
     if(scrollBarIsGrabbed) {
-      var scrollBox = grabbedScrollBar.parentElement;
+      let scrollBox = grabbedScrollBar.parentElement;
       scrollBox.className = scrollBox.className.replace(/\bdisable-select\b/g, '');
     }
-  }
-  function useOverflowScroll(scrollBox) {
-    scrollBox.addEventListener('mouseenter', addOverflowScroll);
-    scrollBox.addEventListener('mouseleave', removeOverflowScroll);
-  }
-  function addOverflowScroll() {
-    if(this.scrollHeight > this.clientHeight)
-      addClass(this, 'fall-back-scrollbox');
-  }
-  function removeOverflowScroll() {
-    this.className = this.className.replace(/\bfall-back-scrollbox\b/g, '');
   }
 
   document.onkeydown = keyScroll;
 
   function keyScroll(e) {
     if(focusedScrollBox != undefined) {
-      var delta;
+      let delta;
       if(e.keyCode == 40) delta = -maxPixelsPerScroll;
       else if(e.keyCode == 38) delta = maxPixelsPerScroll;
       if(delta != null) scrollViaKey(delta);
     }
   }
   function scrollViaKey(delta) {
-    var content = focusedScrollBox.children[1];
+    let content = focusedScrollBox.children[1];
     content.yPos += delta;
     keepScrollInBounds(delta, content);
     smoothScrollIfHighDelta(delta, content);
